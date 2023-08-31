cf423d21935255872e2d4a46ee583912f592c84b
Benefrancis <benefrancis@gmail.com>
2023-08-31 10:29:46 -0300

    Implementamos interface View e EstiloView.java

---
+++ src/main/java/br/com/fiap/domain/view/EstiloView.java
@@ -0,0 +1,61 @@
+package br.com.fiap.domain.view;
+
+import br.com.fiap.domain.entity.Estilo;
+import br.com.fiap.domain.service.EstiloService;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Objects;
+
+public class EstiloView implements View<Estilo, Long> {
+
+    private EstiloService service;
+
+
+    public EstiloView() {
+        service = new EstiloService();
+    }
+
+    @Override
+    public List<Estilo> findAll() {
+        return service.findAll();
+    }
+
+    @Override
+    public Estilo findById(Long id) {
+
+        Long identificador = 0L;
+
+        do {
+            identificador = Long.valueOf( JOptionPane.showInputDialog( "Id do Estilo" ) );
+        } while (identificador > 0);
+
+        return service.findById( identificador );
+    }
+
+    @Override
+    public List<Estilo> findByName(String texto) {
+        String nome = service.valido( texto ) ? texto : JOptionPane.showInputDialog( "Nome do Estilo" );
+        while (!service.valido( nome )) {
+            nome = JOptionPane.showInputDialog( "Nome do Estilo" );
+        }
+        return service.findByName( nome );
+    }
+
+    @Override
+    public Estilo persist(Estilo estilo) {
+        var valido = false;
+        String nome = null;
+        do {
+            nome = JOptionPane
+                    .showInputDialog( "Nome do estilo Musical",
+                            Objects.nonNull( estilo ) &&
+                                    Objects.nonNull( estilo.getNome() ) ? estilo.getNome() : "" );
+        } while (!service.valido( nome ));
+
+        Estilo e = new Estilo();
+        e.setNome( nome );
+
+        return service.persist( e );
+    }
+}
