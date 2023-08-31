24b6308a9611fdba3df94e81c4ef665d0e19dfbb
Benefrancis <benefrancis@gmail.com>
2023-08-31 12:09:01 -0300

    Singleton

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -9,12 +9,14 @@ import br.com.fiap.domain.service.MusicaService;
 import br.com.fiap.domain.view.MenuView;
 
 import javax.swing.*;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         MenuView menu = new MenuView();
         menu.show();
+
 
     }
 }
